31d0963499f46ae958aa6af3b1f4fb78b9545eb2
Mrigakshi Pandey <mrigakshipandey@gmail.com>
2022-04-27 15:34:59 +0530

    completed desktop design for landing about and map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from "highcharts/highmaps";
+import * as worldMap from '@highcharts/map-collection/custom/world.topo.json';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'globetrotter';
+  Highcharts: typeof Highcharts = Highcharts;
+  chartConstructor = "mapChart";
+
+
+  chartOptions: Highcharts.Options = {
+    chart: {
+      map: worldMap
+    },
+    title: {
+      text: ""
+    },
+    tooltip: {
+      enabled: false
+    },
+    colorAxis: {
+      max: 1,
+      maxColor: '#ffaa00'
+    },
+    mapNavigation: {
+      enabled: false,
+      buttonOptions: {
+        alignTo: "spacingBox"
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    legend: {
+      enabled: false
+    },
+    series: [
+      {
+        type: "map",
+        name: "Visited",
+        data: [
+          ['us', 1], //usa
+          ['in', 1], //india
+          ['la', 1], //laos
+          ['za', 1], //south africa
+          ['mu', 1], //mauritius
+          ['th', 1], //thailand
+          ['de', 1]  //germany
+        ],
+        dataLabels: {
+          enabled: true,
+          color: '#000000',
+          formatter: function () {
+            if (this.point.value) {
+              return this.point.name;
+            } else return
+          }
+        },
+        tooltip: {
+          headerFormat: '',
+          pointFormat: '{point.name}'
+        }
+      }
+    ]
+  };
 }
